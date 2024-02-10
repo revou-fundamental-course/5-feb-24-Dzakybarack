d62bffb2a4b67c5e90cd0ef7c9163944fae97b54
Dzakybarack <dzakyabdur425@gmail.com>
2024-02-10 21:58:20 +0700

    progess

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,4 @@
+var nilaiAsliInput;
 function hitung() {
  var input = parseFloat(document.getElementById("input").value);
  var hasil = (input * 9/5) + 32; // Misalnya, konversi dari Celcius ke Fahrenheit
@@ -5,4 +6,25 @@ function hitung() {
  // Menampilkan hasil pada elemen dengan ID "hasil"
  document.getElementById("hasil").innerText =  hasil + " Fahrenheit";
  document.getElementById("proses").innerText = input  + "*9/5 + 32 = " + hasil +" F";
+}
+
+function fungsireset() {
+ var input = document.getElementById("input");
+ nilaiAsliInput = input.value;
+ input.value = "";
+ // Menampilkan hasil pada elemen dengan ID "hasil"
+ document.getElementById("hasil").innerText =  "";
+ document.getElementById("proses").innerText = "";
+}
+
+function reverseInput() {
+ // Mendapatkan referensi ke elemen input
+ var input = document.getElementById("input");
+ // Mengembalikan nilai input ke nilai asli sebelum direset
+ input.value = nilaiAsliInput;
+ var hasil = (nilaiAsliInput * 9/5) + 32; // Misalnya, konversi dari Celcius ke Fahrenheit
+ console.log(hasil);
+ // Menampilkan hasil pada elemen dengan ID "hasil"
+ document.getElementById("hasil").innerText =  hasil + " Fahrenheit";
+ document.getElementById("proses").innerText = nilaiAsliInput  + "*9/5 + 32 = " + hasil +" F";
 }
